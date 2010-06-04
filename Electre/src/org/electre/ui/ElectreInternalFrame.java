fcddfb5fe45c3cfa54862e81453cdea59c5b2db8
caicedo77 <caicedo77@4f6d1957-62f9-2faf-82de-d0ea2adf5db9>
2010-06-04 04:59:55 +0000

    UI iniciada...

--- Electre/src/org/electre/ui/ElectreInternalFrame.java
+++ Electre/src/org/electre/ui/ElectreInternalFrame.java
@@ -64,6 +64,11 @@ public class ElectreInternalFrame extends javax.swing.JInternalFrame {
         jScrollPane1.setViewportView(jTable1);
 
         jButton1.setText(org.openide.util.NbBundle.getMessage(ElectreInternalFrame.class, "ElectreInternalFrame.jButton1.text")); // NOI18N
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -120,6 +125,11 @@ public class ElectreInternalFrame extends javax.swing.JInternalFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+
+    }//GEN-LAST:event_jButton1ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
